3dd75f71d263c21e4085d30a44a39eb46ffa807a
Gamer <gamertechs.defi@gmail.com>
2025-01-12 21:39:41 +0100

    bug fixes 0.3.3

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import WebApp from '@twa-dev/sdk';
 import { initializeUser } from '@/api/firebase/triggers';
